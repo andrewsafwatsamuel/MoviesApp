bb3ef099ec538f0f5a662403adc1926914d25dbd
andrew <andrewsafwatsamuel@gmail.com>
2019-08-29 00:22:22 +0200

    fixed disconnected text

---
+++ app/src/androidTest/java/com/example/moviesapp/features/home/HomeActivityTest.kt
@@ -0,0 +1,12 @@
+package com.example.moviesapp.features.home
+
+import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class HomeActivityTest{
+
+    
+
+}
