aeac76cfa84c49612b77c3a1b07bfeaabbe4ea9a
Andrew <AndrewSafwatSamuel@gmail.com>
2021-05-05 11:34:55 +0200

    implemented MoviesAdapter

---
+++ app/src/main/java/com/example/moviesapp/features/movies/MoviesAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.moviesapp.features.movies
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.Movie
+import com.example.moviesapp.POSTER_SIZE
+import com.example.moviesapp.R
+import com.example.moviesapp.databinding.GridMovieItemBinding
+import com.example.moviesapp.setImageUrl
+
+private fun createUtils() = object : DiffUtil.ItemCallback<Movie>() {
+    override fun areItemsTheSame(oldItem: Movie, newItem: Movie) = oldItem == newItem
+
+    override fun areContentsTheSame(oldItem: Movie, newItem: Movie) = oldItem.id == newItem.id
+}
+
+class MovieViewHolder(
+    private val binding: GridMovieItemBinding
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(item: Movie) {
+        binding.gridMovieBoaster.setImageUrl(POSTER_SIZE,item.poster)
+    }
+}
+
+class MoviesPagingAdapter(
+    private val doOnClick: (Movie) -> Unit
+) : PagingDataAdapter<Movie, MovieViewHolder>(createUtils()) {
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) = with(holder) {
+        val item = getItem(position) ?: return@with
+        bind(item)
+        itemView.setOnClickListener { doOnClick(item) }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = LayoutInflater
+        .from(parent.context)
+        .inflate(R.layout.grid_movie_item, parent, false)
+        .let(GridMovieItemBinding::bind)
+        .let(::MovieViewHolder)
+}
