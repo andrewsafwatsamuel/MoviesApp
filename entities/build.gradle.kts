66345bebf6e510d500d723eda6d639ddf0dbe238
Andrew Safwat <andrewsafwatsamuel@gmail.com>
2023-10-21 22:58:21 +0300

    migrated entities gradle file

---
+++ entities/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-parcelize")
+    id("kotlin-android")
+}
+android {
+    buildToolsVersion = "34.0.0"
+    compileSdk = 34
+    namespace = "com.example.entities"
+    defaultConfig {
+        minSdk = 21
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+}
+
+dependencies {
+    implementation(files(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
+    testImplementation("junit:junit:4.13.2")
+    api("androidx.room:room-runtime:2.6.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.10")
+    implementation("com.google.code.gson:gson:2.10.1")
+
+}
