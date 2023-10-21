15c9a79fefab7719d8697a6c2215ef0df6202d3b
Andrew Safwat <andrewsafwatsamuel@gmail.com>
2023-10-21 22:13:48 +0300

    updated gradle structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,35 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+val room_version by extra { "2.6.0" }
+val kotlin_version by extra { "1.9.10" }
+val junit_version by extra { "4.13.2" }
+val build_tools_version by extra { "34.0.0" }
+val version_name by extra { "1.0" }
+val app_varsion_code by extra { 1 }
+val compile_version by extra { 34 }
+val minimum_version by extra { 21 }
 
-buildscript {
-    extra.apply {
-        set("agp_version", "8.1.2")
-        set("room_version", "2.6.0")
-        set("kotlin_version", "1.9.10")
-        set("junit_version", "4.13.2")
-        set("build_tools_version", "34.0.0")
-        set("version_name", "1.0")
-        set("app_varsion_code", 1)
-        set("compile_version", 34)
-        set("minimum_version", 21)
-    }
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:${project.extra.get("agp_version")}")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${project.extra.get("kotlin_version")}")
-        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.10-1.0.13")
-        // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
+plugins {
+    id("com.android.application") version "8.1.1" apply false
+    id("com.android.library") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
+    kotlin("jvm") version "1.9.10"
 }
---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+rootProject.name = "MoviesApp"
+include(":app")
+include(":entities")
+include(":domain")
