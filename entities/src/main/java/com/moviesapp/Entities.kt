809185188b09b355d2a207403651391707a04406
Andrew Safwat <andrewsafwatsamuel@gmail.com>
2023-11-09 19:35:55 +0200

    update MovieResponse, Movie models

--- entities/src/main/java/com/moviesapp/Entities.kt
+++ entities/src/main/java/com/moviesapp/Entities.kt
@@ -3,30 +3,61 @@ package com.moviesapp
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.JsonNames
 import java.io.Serializable
 
-data class MovieResponse(
-    @field:SerializedName("page") val pageNumber: Int,
-    @field:SerializedName("total_results") val resultCount: Int,
-    @field:SerializedName("total_pages") val pageCount: Int,
-    @field:SerializedName("results") val results: List<Movie>
+@kotlinx.serialization.Serializable
+data class MovieResponse @OptIn(ExperimentalSerializationApi::class) constructor(
+    @JsonNames("page")
+    @field:SerializedName("page")
+    val pageNumber: Int,
+    @JsonNames("total_results")
+    @field:SerializedName("total_results")
+    val resultCount: Int,
+    @JsonNames("total_pages")
+    @field:SerializedName("total_pages")
+    val pageCount: Int,
+    val results: List<Movie>
 ) : Serializable
 
-data class Movie(
-    @field:SerializedName("vote_count") val voteCount: Int?,
-    @field:SerializedName("id") val id: Long?,
-    @field:SerializedName("video") val haveVideo: Boolean?,
-    @field:SerializedName("vote_average") val voteAverage: Double?,
-    @field:SerializedName("title") val title: String?,
-    @field:SerializedName("popularity") val popularity: Float?,
-    @field:SerializedName("poster_path") val poster: String?,
-    @field:SerializedName("original_language") val language: String?,
-    @field:SerializedName("original_title") val originalTitle: String?,
-    @field:SerializedName("genre_ids") val genreIds: List<Int>?,
-    @field:SerializedName("backdrop_path") val backDropPhoto: String?,
-    @field:SerializedName("adult") val isAdult: Boolean?,
-    @field:SerializedName("overview") val overView: String?,
-    @field:SerializedName("release_date") val releaseDate: String?
+@kotlinx.serialization.Serializable
+data class Movie @OptIn(ExperimentalSerializationApi::class) constructor(
+    @JsonNames("vote_count")
+    @field:SerializedName("vote_count")
+    val voteCount: Int?,
+    val id: Long?,
+    @JsonNames("video")
+    @field:SerializedName("video")
+    val haveVideo: Boolean?,
+    @JsonNames("vote_average")
+    @field:SerializedName("vote_average")
+    val voteAverage: Double?,
+    val title: String?,
+    val popularity: Float?,
+    @JsonNames("poster_path")
+    @field:SerializedName("poster_path")
+    val poster: String?,
+    @JsonNames("original_language")
+    @field:SerializedName("original_language")
+    val language: String?,
+    @JsonNames("original_title")
+    @field:SerializedName("original_title")
+    val originalTitle: String?,
+    @JsonNames("genre_ids")
+    @field:SerializedName("genre_ids")
+    val genreIds: List<Int>?,
+    @JsonNames("backdrop_path")
+    @field:SerializedName("backdrop_path")
+    val backDropPhoto: String?,
+    @JsonNames("adult")
+    @field:SerializedName("adult")
+    val isAdult: Boolean?,
+    @JsonNames("overview")
+    val overView: String?,
+    @JsonNames("release_date")
+    @field:SerializedName("release_date")
+    val releaseDate: String?
 ) : Serializable
 
 data class DetailsResponse(
@@ -97,9 +128,9 @@ data class Video(
 data class CreditsResponse(
     @field:SerializedName("cast") val cast: List<CastMember?>,
     @field:SerializedName("crew") val crew: List<CrewMember?>
-):Serializable
+) : Serializable
 
-data class CreditsMember(val title:String?,val role:String?,val url:String?)
+data class CreditsMember(val title: String?, val role: String?, val url: String?)
 
 data class CastMember(
     @field:SerializedName("cast_id") val castId: Int?,
@@ -110,7 +141,8 @@ data class CastMember(
     @field:SerializedName("name") val name: String?,
     @field:SerializedName("order") val order: Int?,
     @field:SerializedName("profile_path") val photo: String?
-):Serializable
+) : Serializable
+
 data class CrewMember(
     @field:SerializedName("credit_id") val creditId: String?,
     @field:SerializedName("department") val department: String?,
@@ -119,7 +151,7 @@ data class CrewMember(
     @field:SerializedName("job") val job: String?,
     @field:SerializedName("name") val name: String?,
     @field:SerializedName("profile_path") val photo: String?
-):Serializable
+) : Serializable
 
 @Entity
 data class SuccessfulQuery(
