f25b50b297858af340774e4f61acd3386526904b
Andrew Safwat <andrewsafwatsamuel@gmail.com>
2023-11-10 00:17:16 +0200

    fixed exception from ResponseToSingleMapper

--- data_sources/src/main/java/com/example/data_sources/repositories/ResponseToSingleMapper.kt
+++ data_sources/src/main/java/com/example/data_sources/repositories/ResponseToSingleMapper.kt
@@ -2,6 +2,7 @@ package com.example.data_sources.repositories
 
 import com.example.Response
 import io.reactivex.Single
+import io.reactivex.SingleEmitter
 import kotlinx.coroutines.runBlocking
 
 class ResponseToSingleMapper {
@@ -9,14 +10,15 @@ class ResponseToSingleMapper {
     inline operator fun <T> invoke(crossinline apiCall: suspend () -> Response<T>): Single<T> =
         Single.create { emitter ->
             try {
-                runBlocking { apiCall() }.apply {
-                    takeIf { statusCode in 200..399 }
-                        ?.body?.let(emitter::onSuccess)
-                        ?: throw RuntimeException("Error has occurred: Status code: $statusCode\n$description")
-                }
+                runBlocking { apiCall() }.emitOnSingleEmitter(emitter)
             } catch (t: Throwable) {
-                emitter.onError(t)
+                if (!emitter.isDisposed) emitter.onError(t)
             }
         }
+
+    fun <T> Response<T>.emitOnSingleEmitter(emitter: SingleEmitter<T>) {
+        if (statusCode !in 200..399) throw RuntimeException("Error has occurred: Status code: $statusCode\n$description")
+        if (!emitter.isDisposed) body?.let(emitter::onSuccess)
+    }
 
 }
