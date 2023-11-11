492aa6c860af6abb9d39be8203ac0149524f2556
Andrew Safwat <andrewsafwatsamuel@gmail.com>
2023-11-11 04:59:48 +0200

    applied Connectivity check interceptor

--- data_sources/src/main/java/com/example/data_sources/server_gateway/HttpClient.kt
+++ data_sources/src/main/java/com/example/data_sources/server_gateway/HttpClient.kt
@@ -1,13 +1,17 @@
 package com.example.data_sources.server_gateway
 
+import DataSources
+import com.example.NotConnectedException
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.DefaultRequest
+import io.ktor.client.plugins.HttpSend
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.logging.DEFAULT
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.plugin
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
@@ -16,7 +20,7 @@ const val API_KEY_VALUE = "e0e450709bf35bc741a26a4ffd9e3116"
 const val API_KEY_NAME = "api_key"
 
 internal val httpClient = HttpClient(CIO) {
-    install(DefaultRequest){
+    install(DefaultRequest) {
         url(BASE_URL)
         headers.apply {
             append("Content-type", "application/json")
@@ -30,11 +34,16 @@ internal val httpClient = HttpClient(CIO) {
         logger = Logger.DEFAULT
         level = LogLevel.ALL
     }
-    install(ContentNegotiation){
+    install(ContentNegotiation) {
         json(Json {
             prettyPrint = true
             isLenient = true
             ignoreUnknownKeys = true
         })
+    }
+}.apply {
+    plugin(HttpSend).intercept { request ->
+        if (DataSources.networkStatus?.isConnected() == false) throw NotConnectedException()
+        execute(request)
     }
 }
--- entities/src/main/java/com/moviesapp/Exceptions.kt
+++ entities/src/main/java/com/moviesapp/Exceptions.kt
@@ -1,14 +1,5 @@
 package com.moviesapp
 
-const val NOT_Null = "can not be null"
-const val PAGINATION = "PaginationScrollListener"
+import io.ktor.utils.io.errors.IOException
 
-fun illegalState(cause: String): Nothing = throw IllegalStateException(cause)
-
-fun nullQueryParameters(): Nothing = illegalState("$PAGINATION queryParameters $NOT_Null")
-
-fun nullLifeCycleOwner(): Nothing = illegalState("$PAGINATION lifeCycleOwner $NOT_Null")
-
-fun nullLayoutManager(): Nothing = illegalState("$PAGINATION layoutManager $NOT_Null")
-
-fun nullRetrieve(): Nothing = illegalState("$PAGINATION retrieve must have implementation")
+class NotConnectedException : IOException("Not connected")
